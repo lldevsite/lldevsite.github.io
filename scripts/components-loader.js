50d06d1262ca1db0899a32807392bb9e0db5db4c
coffeeculturedesign <lldevsite@gmail.com>
2024-10-27 21:42:38 +0100

    card slider update

--- scripts/components-loader.js
+++ scripts/components-loader.js
@@ -6,4 +6,14 @@ function load(comp, where, obj) {
             document.getElementById(where).innerHTML += template(obj);
         })
         .catch((e) => console.error(e));
+}
+
+function loadInFront(comp, where, obj) {
+    fetch(`${comp}`)
+        .then((res) => res.text())
+        .then((text) => {
+            let template = new Function('obj', 'return `' + text + '`;');
+            document.getElementById(where).innerHTML = template(obj) + document.getElementById(where).innerHTML;
+        })
+        .catch((e) => console.error(e));
 }   
